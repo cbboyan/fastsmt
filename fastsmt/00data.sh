cf9cf83ce0e61bd025a4f7ccfb501c795a62f98d
Jan Jakubuv <jakubuv@gmail.com>
2025-08-11 13:57:39 +0000

    chore: experiment run scripts

---
+++ fastsmt/00data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python scripts/py/dataset_create.py --split "50 20 30" --benchmark_dir examples/QF_NIA/leipzig
+
---
+++ fastsmt/01select.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cat eval/synthesis/leipzig_example/train/2/strategies.txt | shuf | head -10 > sample_strategies.txt
+
