b223580060ea5be64d7b42bd81ad4a8c8f253315
Jan Jakubuv <jakubuv@gmail.com>
2025-08-11 15:29:20 +0200

    chore: experiment files

---
+++ fastsmt/valid.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python scripts/py/validate.py \
+        --strategy_file output_strategy.txt \
+        --benchmark_dir examples/QF_NIA/leipzig/test \
+        --max_timeout 10 \
+        --batch_size 4
